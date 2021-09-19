93018fbffe649c1afc413c021b753de83c744cac
David Morales <david@davidmles.com>
2021-09-19 20:08:30 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Tailwind CSS fundamentals
+
+In this repository you can find the final *index* file after finishing each lesson of the course:
+
+- Building a page from scratch: *index.html*
+- Making the page responsive: *index-responsive.html*
+- Applying CSS states: *index-states.html*
+- Adding dark mode: *index-dark.html*
+- Classes reuse with @apply: *index-apply.html*
+- Using the Typography plugin: *index-typography.html*
+- Using the Forms plugin: *index-forms.html*
+- Using the Aspect Ratio plugin: *index-aspect-ratio.html*
+- Using the Line Clamp plugin: *index-line-clamp.html*
+
+The rest of the files are what they would look like after completing the course:
+
+- *package.json*
+- *tailwind.config.js*
+- *img* directory (where all images are located)
+- *src/tailwind.css*
---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './*.html'
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        dark: {
+          css: {
+            color: theme('colors.gray.400'),
+            '[class~="lead"]': {
+              color: theme('colors.gray.300'),
+            },
+            a: {
+              color: theme('colors.blue.400'),
+            },
+            strong: {
+              color: theme('colors.white'),
+            },
+            'ul > li::before': {
+              backgroundColor: theme('colors.gray.600'),
+            },
+            h2: {
+              color: theme('colors.white'),
+            },
+            'figure figcaption': {
+              color: theme('colors.gray.400'),
+            },
+            thead: {
+              color: theme('colors.white'),
+              borderBottomColor: theme('colors.gray.400'),
+            },
+            'tbody tr': {
+              borderBottomColor: theme('colors.gray.600'),
+            },
+          },
+        },
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      typography: ['dark'],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
